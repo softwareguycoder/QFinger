b9324cf45cb3aea5bacfe6b36d67c106af1d2d1f
Brian Hart <brian.hart@xylogix.net>
2018-09-12 13:39:52 -0400

    12 Sep 2018 Skeleton application code with entry point

---
+++ QFinger/QFinger.c
@@ -0,0 +1,10 @@
+#include <winsock.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int PASCAL WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, INT nCmdShow)
+{
+	// TODO: Add the application's code here
+	return 0;
+}
